f38c93de83605e87d0beb4fbb36394d2964cae24
NoviceCoderInfinity <anupamscience06@gmail.com>
2024-12-23 14:28:34 +0000

    Added Soln for Dynamic Programming / Coin Combinations II

---
+++ Dynamic_Programming/Coin_Combinations_II.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int num_coins, target; cin >> num_coins >> target;
+	vector<int> coins_psbl;
+	for (int i = 0; i < num_coins; ++i) {
+		int c; cin >> c; coins_psbl.push_back(c);
+	}
+	
+	int MODULUS = pow(10, 9) + 7;
+
+	vector<int> dp(target + 1, 0); dp[0] = 1;
+	for (int coin : coins_psbl) {
+        	for (int i = coin; i <= target; ++i) {
+			dp[i] = (dp[i] + dp[i - coin]) % MODULUS;
+        	}
+	}
+	cout << dp.back() << endl;
+	return 0;
+}
+
